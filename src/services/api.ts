6fdbbdc6974b4ef1e2fc9850f929cb322de6b504
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-03-06 00:18:05 -0300

    chore: fix all ESLint errors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
-import { create } from 'axios'
 import env from '@/config/env'
 
 const BASE_URL = env.baseUrl
-export const api = create({
-  baseURL: BASE_URL,
-})
+export const api = async (url: string) => {
+  return await fetch(`${BASE_URL}${url}`)
+}
